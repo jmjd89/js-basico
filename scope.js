0be0c67020be246c4f676463f7996e3852109ec6
Juan Jimenez <jimenezduranjm@gmail.com>
2021-07-21 14:51:23 -0500

    Se sube scope.js

---
+++ scope.js
@@ -0,0 +1,14 @@
+
+/**
+ * Variable global
+ */
+var primerNombre = "Juan";
+
+/**
+ * Variable local
+ */
+function fun()
+{
+    var apellido = "Jiménez";
+    console.log(primerNombre + " " + apellido);
+}
