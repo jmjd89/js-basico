887a857678dd43b8c6a3e62439c9e8e049c36588
Juan Jimenez <jimenezduranjm@gmail.com>
2021-07-21 23:21:09 -0500

    Se suben avances

---
+++ if.js
@@ -0,0 +1,37 @@
+
+if(true)
+{
+    console.log("Hola");
+}
+else{
+    console.log("Soy falso");
+}
+
+if(false)
+{
+    console.log("Hola");
+}
+else{
+    console.log("Soy falso")
+}
+
+var edad = 23;
+
+if (edad === 18)
+{
+    console.log("Puedes votar, será tu primera votación");
+}
+else if(edad > 18)
+{
+    console.log("Puedes votar de nuevo");
+}
+else{
+    console.log("Aún no puedes votar");
+}
+
+var numero = 19;
+numero > 20 ? console.log("Mayor a 20") : console.log("Menor a 20");
+
+var numero2 = 1;
+var resultado = numero2 === 1 ? "Es un uno" : "No es un uno";
+console.log(resultado);
