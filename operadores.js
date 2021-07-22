a5673a4e09441814e40ec14123d47b94619bab8a
Juan Jimenez <jimenezduranjm@gmail.com>
2021-07-21 20:12:59 -0500

    Se sube operadores.js

---
+++ operadores.js
@@ -0,0 +1,72 @@
+/**
+ * Operadores
+ */
+
+/**
+ * Operadores Binarios: + * - /
+ * Se le dicen binarios porque operan dos valores.
+ * + se usa para sumar numeros y concatenar cadenas.
+ * Operadores Unitarios: Operan con un solo valor.
+ * ! es una negación
+ */
+
+ !false
+//  true
+
+/**
+ * Operadores de asignación
+ * Asignar un valor a una variable
+ */
+
+var e = 12;
+
+/**
+ * OPERADORES DE COMPARACIÓN
+ * == Compara el valor más no el tipo del valor, e contenido
+ */
+
+ 3 == "3"
+//  true
+
+/**
+ * === Compara el tipo de valor y el valor en sí
+ */
+
+ 3 === "3"
+//  false
+
+/**
+ * && los dos operandos son true
+ * || cualquiera de los dos operandos es true
+ */
+
+ var a = true;
+//  undefined
+ var b = false;
+//  undefined
+//  a && b
+//  false
+//  a || b
+//  true
+
+
+/** var a = 50;
+ * a++;
+ * El incremento sucede después
+ */
+
+var a = 50;
+//  undefined
+//  a++;
+//  50
+
+/** 
+ * var a = 50;
+ * ++a;
+ * El incremento ocurre en la misma linea
+ */
+
+var a = 50;
+//  undefined
+//  ++a;
+//  51  
