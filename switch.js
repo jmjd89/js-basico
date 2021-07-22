a20adf33bb183e5be1f6a88b220cc4d8f53f4c32
Juan Jimenez <jimenezduranjm@gmail.com>
2021-07-22 12:16:15 -0500

    Se suen cambios

---
+++ switch.js
@@ -0,0 +1,26 @@
+/**
+ * Switch:
+ * Funciona por casos.
+ */
+
+var numero = 1;
+
+switch (numero) {
+    
+    case 1:
+        console.log("Soy un uno");
+        break;
+    
+    case 10:
+        console.log("Soy un 10");
+        break;
+    
+    case 100:
+        console.log("Soy un 100");
+        break;
+
+    default:
+        console.log("Soy algún otro número");
+        break;
+
+}
